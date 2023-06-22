6d71136ef0288338dae67bc22a0fc6ad23ef5ee4
TR Murphy <biffsocko@gmail.com>
2023-06-22 15:21:24 -0400

    Update webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+############################################
+# TR Murphy
+# webhook.sh
+#
+# simple script for demonstrating a webhook
+############################################
 
 WEBHOOK="https://someorg.webhook.office.com/webhookb2/0027d8ad-69ee-4be6-9af6-abde58478896@d1bdddc1-de7b-4a77-914e-213d203667f2/IncomingWebhook/ef065e6da6584765b54745c5f55b47f8/cf7cb2e8-d857-424f-9700-516f06c11637"
 
